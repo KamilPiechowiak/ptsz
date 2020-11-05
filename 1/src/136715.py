9e7ee0593afc25758d2b89a2d519b73b31d4454d
ahalamska <a.halamska@gmail.com>
2020-11-05 16:23:19 +0100

    136715 change

--- 1/src/136715.py
+++ 1/src/136715.py
@@ -1,70 +1,70 @@
 #!/usr/bin/env python3
 import sys
 
-LENGTH = 0
-READY = 1
-DUE = 2
-WEIGHT = 3
-
-max_due = 0
-max_weight = 0
-
-
-def read_files(instance_file: str):
-    instance_file = open(instance_file, "r+")
-    return int(instance_file.readline().rstrip()), [[int(x) for x in line.strip().split(' ')] for line in
-                                                    instance_file.readlines()]
+u = 0
+t = 1
+N = 2
+q = 3
+A = 0
+C = 0
 
 
-def find_next(jobs, current_time, ranked_jobs):
-    candidate = None
-    max_function_val = -1000
-    late_jobs = []
+def read_files(w: str):
+    w = open(w, "r+")
+    return int(w.readline().rstrip()), [[int(x) for x in line.strip().split(' ')] for line in w.readlines()]
+
+
+def find_next(jobs, y, L):
+    p = None
+    b = -1000
+    n = []
     for i in range(len(jobs)):
-        if i + 1 not in ranked_jobs and current_time > jobs[i][READY]:
-            function_val = 2 * jobs[i][WEIGHT] / max_weight - jobs[i][DUE] / max_due
-            if current_time + jobs[i][LENGTH] > jobs[i][DUE]:
-                late_jobs.append(i)
-            elif function_val > max_function_val:
-                max_function_val = function_val
-                candidate = i
+        if i + 1 not in L and y > jobs[i][t]:
+            d = 2 * jobs[i][q] / C - jobs[i][N] / A
+            if y + jobs[i][u] > jobs[i][N]:
+                n.append(i)
+            elif d > b:
+                b = d
+                p = i
+    if p is None and n.__len__() != 0:
+        p = n.pop()
+    return p
 
-    if candidate is None and late_jobs.__len__() != 0:
-        candidate = late_jobs.pop()
-    return candidate
 
-
-def rank_jobs(jobs_count, jobs):
-    global max_due, max_weight
+def rank_jobs(l, jobs):
+    global A, C
     for i in range(len(jobs)):
-        if jobs[i][DUE] > max_due:
-            max_due = jobs[i][DUE]
-        if jobs[i][WEIGHT] > max_weight:
-            max_weight = jobs[i][WEIGHT]
-    ranked_jobs = []
-    current_time = 0
-    result_criteria = 0
-    while jobs_count != ranked_jobs.__len__():
-        candidate_index = find_next(jobs, current_time, ranked_jobs)
-        if candidate_index is None:
-            current_time += 1
+        if jobs[i][N] > A:
+            A = jobs[i][N]
+        if jobs[i][q] > C:
+            C = jobs[i][q]
+    L = []
+    y = 0
+    J = 0
+    while l != L.__len__():
+        r = find_next(jobs, y, L)
+        if r is None:
+            y += 1
         else:
-            current_time += jobs[candidate_index][LENGTH]
-            if current_time > jobs[candidate_index][DUE]:
-                result_criteria += jobs[candidate_index][WEIGHT]
-            ranked_jobs.append(candidate_index + 1)
-    return result_criteria, ranked_jobs
+            y += jobs[r][u]
+            if y > jobs[r][N]:
+                J += jobs[r][q]
+            L.append(r + 1)
+    return J, L
 
 
 def main(argv):
-    instance_file = argv[0]
-    result_file = open(f'seq.out', 'w+')
-    jobs_count, jobs = read_files(instance_file)
-    criteria, ranked_jobs = rank_jobs(jobs_count, jobs)
-    result_file.write(f'{criteria.__str__()}\n')
-    for i in range(len(ranked_jobs)):
-        result_file.write(f'{ranked_jobs[i].__str__()} ')
-    result_file.close()
+    f = []
+    a = int(input())
+    for i in range(a):
+        v = input().split(" ")
+        f.append([int(x) for x in v])
+    M, L = rank_jobs(a, f)
+    sys.stdout.write(M.__str__())
+    sys.stdout.write("\n")
+    for i in range(len(L)):
+        sys.stdout.write(L[i].__str__())
+        sys.stdout.write(" ")
 
 
 if __name__ == "__main__":
