a3fab0f2c27c624438a0581aba3edca9a0a3bf69
danieluss <danieluss@protonmail.com>
2020-11-05 16:06:21 +0100

    136775

--- metadata.py
+++ metadata.py
@@ -25,8 +25,9 @@ program_commands = [
         "136811": "1/src/136811_solver",
         "136697": "python3 1/src/solver_136697.py",
         "136705": "python3 1/src/136705.py",
-        "136810": "python3 1/src/136810/main.py"
-	"136573": "python3 1/src/solver136573.py"
-	"136823": "python3 1/src/136823_serializer.py"
+        "136810": "python3 1/src/136810/main.py",
+	    "136573": "python3 1/src/solver136573.py",
+	    "136823": "python3 1/src/136823_serializer.py",
+        "136775": "python3 1/src/algorithm136775.py"
     }
 ]
