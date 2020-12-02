52864decb311f2b9444568d540d103b264e03c6e
Błażej <baryla181@gmail.com>
2020-12-02 11:20:52 +0100

    Algorithm fix

--- p2/src/id126828/algorithm.py
+++ p2/src/id126828/algorithm.py
@@ -15,25 +15,36 @@ class Algorithm126828(Algorithm):
                 yield l[i * newn:i * newn + newn]
             yield l[n * newn - newn:]
 
-        sorted_tasks = sorted(enumerate(in_data.tasks, 1), key=lambda x: x[1].duration,reverse=True)
-        machines = sorted(enumerate(in_data.machine_speeds), key=lambda x: x[1])
-        # split tasks into equal parts to machines
-        to_machines = chunks(sorted_tasks, in_data.no_machines)
-        schedule = [[]] * in_data.no_machines
-        local_solutions = [0.0] * in_data.no_machines
-        for machine_num, machine_speed in machines:
-            for tasks in to_machines:
-                # for every task use timer to count solution and add
-                # add this tasks  to this machine
-                local_timer = 0
+        def find_best(task_checked):
+            solutions = {}
+            timer = [0] * in_data.no_machines
+            for machine_num, machine in machines:
+                timer[machine_num] = max(local_timers[machine_num], task_checked.ready)
+                timer[machine_num] += task_checked.duration * machine
+                solutions[machine_num] = timer[machine_num] - task.ready
+            #choose best machine
+            chosen_machine = min(solutions, key=solutions.get)
+            #increment timer and solution on local chosen machine
+            local_solutions[chosen_machine] += solutions[chosen_machine]
+            local_timers[chosen_machine] = timer[chosen_machine]
 
-                for ind, task in tasks:
-                    schedule[machine_num].append(ind)
-                    local_timer = max(local_timer, task.ready)
-                    local_timer += task.duration * machine_speed
-                    local_solutions[machine_num] += local_timer - task.ready
-                    # add to current machine task with id.
+            return chosen_machine
+
+        sorted_tasks = sorted(enumerate(in_data.tasks, 1), key=lambda x: x[1].ready)
+        machines = list(enumerate(in_data.machine_speeds))
+        # split tasks into equal parts to machines
+        local_solutions = [0.0] * in_data.no_machines
+        local_timers = [0.0] * in_data.no_machines
+        local_schedules = []
+        [local_schedules.append([]) for i in range(5)]
+        #for every task find where is minimum value and add this to schedule.
+        for ind, task in sorted_tasks:
+            #find best machine for this solution
+            append_task_to = find_best(task)
+            #append this tasks to chosen machine
+            local_schedules[append_task_to].append(ind)
+
 
         global_solution = sum(local_solutions) / in_data.no_tasks
         return Solution(score=global_solution,
-                        schedule=Schedule(n=in_data.no_tasks, m=in_data.no_machines, schedule=schedule))
+                        schedule=Schedule(n=in_data.no_tasks, m=in_data.no_machines, schedule=local_schedules))
