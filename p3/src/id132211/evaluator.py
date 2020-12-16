c2b69675c3a3ca2629024d7a76ceb7f24a39a763
RobertDudek <robert.dudek@student.put.poznan.pl>
2020-12-16 23:31:27 +0100

    132211 instances, gen, eval, basic algo

---
+++ p3/src/id132211/evaluator.py
@@ -0,0 +1,21 @@
+from p3.properties import EPS
+from p3.src.data_api import Instance, Solution
+from p3.src.evaluator_api import EvaluatorOutput, Evaluator
+
+
+class Evaluator132211(Evaluator):
+
+    def evaluate(self, in_data: Instance, output: Solution, time: float = None) -> EvaluatorOutput:
+        score = 0.0
+        moments = [0 for _ in range(in_data.no_machines)]
+        for task_index in output.schedule:
+            task = in_data.tasks[task_index-1]
+            moments[0] += task.duration[0]
+            moments[1] = max(moments[0], moments[1]) + task.duration[1]
+            moments[2] = max(moments[1], moments[2]) + task.duration[2]
+            score += max(0, moments[2] - task.due_date)*task.weight
+        score /= sum([task.weight for task in in_data.tasks])
+        correct = abs(score - output.score) <= EPS
+        if score != 0:
+            correct = correct or abs(score - output.score)/score <= EPS
+        return EvaluatorOutput(correct, score, time)
