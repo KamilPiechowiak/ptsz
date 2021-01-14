72233c5b906a3e6e10b4f87e028a55056db3c287
jdomeracki <j.domeracki96@gmail.com>
2021-01-14 12:42:39 +0100

    Minified algorithm added

--- p3/src/id127183/algorithm.py
+++ p3/src/id127183/algorithm.py
@@ -1,34 +1,28 @@
 from p3.src.algorithm_api import Algorithm
-from p3.src.data_api import Instance, Solution, Schedule
+from p3.src.data_api import Instance,Solution,Schedule
 from p3.src.id127183.evaluator import Evaluator127183
-
-
 class Job:
-
-    def __init__(self, id, duration, due_date, weight):
-        self.id = id
-        self.duration = duration
-        self.due_date = due_date
-        self.weight = weight
-
-
+ def __init__(p,id,W,i,S):
+  p.id=id
+  p.duration=W
+  p.due_date=i
+  p.weight=S
 class Algorithm127183(Algorithm):
-
-    def H2I(self, jobs):
-        J1, J2 = [], []
-        for job in jobs:
-            p1, p2, p3 = job.duration
-            if p1 <= p2 + p3:
-                J1.append(job)
-            else:
-                J2.append(job)
-        J1.sort(key=lambda x: (x.duration[0] * float(x.due_date/x.weight)))
-        J2.sort(key=lambda x: (x.duration[1] + x.duration[2]) * float(x.weight/(x.due_date+0.00000001)), reverse=True)
-        return J1, J2
-
-    def run(self, in_data: Instance) -> Solution:
-        jobs = [Job(counter+1, task.duration, task.due_date, task.weight) for counter, task in enumerate(in_data.tasks)]
-        jobs.sort(key=lambda x: (sum(x.duration) * float(x.due_date/x.weight)))
-        schedule = Schedule(in_data.no_tasks, [job.id for job in jobs])
-        score = Evaluator127183().evaluate(in_data, Solution(0.0, schedule)).value
-        return Solution(score, schedule)
+ def H2I(p,jobs):
+  J1,J2=[],[]
+  for v in jobs:
+   p1,p2,p3=v.duration
+   if p1<=p2+p3:
+    J1.append(v)
+   else:
+    J2.append(v)
+  J1.sort(key=lambda x:(x.duration[0]*float(x.due_date/x.weight)))
+  J2.sort(key=lambda x:(x.duration[1]+x.duration[2])*float(x.weight/(x.due_date+0.00000001)),reverse=True)
+  return J1,J2
+ def run(p,in_data:Instance)->Solution:
+  jobs=[Job(counter+1,task.duration,task.due_date,task.weight)for counter,task in enumerate(in_data.tasks)]
+  jobs.sort(key=lambda x:(sum(x.duration)*float(x.due_date/x.weight)))
+  J=Schedule(in_data.no_tasks,[v.id for v in jobs])
+  s=Evaluator127183().evaluate(in_data,Solution(0.0,J)).value
+  return Solution(s,J)
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
--- p3/src/id127183/evaluator.py
+++ p3/src/id127183/evaluator.py
@@ -1,26 +1,24 @@
-from p3.src.data_api import Instance, Solution
-from p3.src.evaluator_api import EvaluatorOutput, Evaluator
+from p3.src.data_api import Instance,Solution
+from p3.src.evaluator_api import EvaluatorOutput,Evaluator
 from p3.properties import EPS
-
-
 class Evaluator127183(Evaluator):
-
-    def evaluate(self, in_data: Instance, output: Solution, time: float = None) -> EvaluatorOutput:
-        currMoments = [0, 0, 0]
-        score, denom = 0, 0
-        for currTaskIdx in output.schedule:
-            currentTask = in_data.tasks[currTaskIdx - 1]
-            for index, machineMoment in enumerate(currMoments):
-                if index == 0:
-                    currMoments[index] += currentTask.duration[index]
-                else:
-                    possiblePartTaskStart = max(currMoments[index-1], currMoments[index])
-                    currMoments[index] = possiblePartTaskStart + currentTask.duration[index]
-            endMoment = max(currMoments)
-            lateness = max(0, endMoment - currentTask.due_date)
-            score += lateness * currentTask.weight
-            denom += currentTask.weight
-        score /= denom
-        correct = abs(score - output.score) <= EPS
-        value = score
-        return EvaluatorOutput(correct, value, time)
+ def evaluate(self,in_data:Instance,output:Solution,time:float=None)->EvaluatorOutput:
+  P=[0,0,0]
+  D,F=0,0
+  for T in output.schedule:
+   O=in_data.tasks[T-1]
+   for A,machineMoment in enumerate(P):
+    if A==0:
+     P[A]+=O.duration[A]
+    else:
+     w=max(P[A-1],P[A])
+     P[A]=w+O.duration[A]
+   H=max(P)
+   Q=max(0,H-O.due_date)
+   D+=Q*O.weight
+   F+=O.weight
+  D/=F
+  C=abs(D-output.score)<=EPS
+  E=D
+  return EvaluatorOutput(C,E,time)
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
