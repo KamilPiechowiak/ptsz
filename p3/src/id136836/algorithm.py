df2e9a96e996276d11b3b15f8979de9fdc26500d
anitazielinska <anita.zielinska@student.put.poznan.pl>
2021-01-14 15:07:27 +0100

    136836 algo

--- p3/src/id136836/algorithm.py
+++ p3/src/id136836/algorithm.py
@@ -2,7 +2,8 @@ from p3.src.algorithm_api import Algorithm
 from p3.src.data_api import Instance, Solution, Schedule
 
 import random
-
+def priority(a, b, w, d):
+    return a * w - b * d
 
 def score(tasks_list: list) -> int:
     D, w = 0, 0
@@ -36,22 +37,14 @@ class Algorithm136836(Algorithm):
         end_tasks = []
         machine_time = m * [0]
         tasks = [i for i in zip(range(1, n+1), in_data.tasks)]
+        result = []
 
-
-        best = tasks.copy()
-        for _ in range(100):
-            shuffle = best.copy()
-            random.shuffle(shuffle)
-            if score(shuffle) < score(best):
-                best = shuffle
-
-        for _ in range(100):
-            swap = best.copy()
-            i, j = random.randint(0, n-1), random.randint(0, n-1)
-            best[i], best[j] = best[j], best[i]
-            if score(swap) < score(best):
-                best = swap
-
+        best = tasks
+        for a in range(1, 14, 2):
+            for b in range(5):
+                tmp = sorted(tasks, key=lambda task: - priority(a, b, task[1].weight, task[1].due_date))
+                if score(tmp) < score(best):
+                    best = tmp
 
         schedule = best
         D = score(schedule)
